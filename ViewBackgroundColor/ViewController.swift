83f7bf17644e9c8e9f30f2504a0b9297f1a8cc96
ruscodecreate <cpabuild@gmail.com>
2020-10-15 20:53:03 +0100

    Initial Commit

---
+++ ViewBackgroundColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewBackgroundColor
+//
+//  Created by Владислав Крисько on 15/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
